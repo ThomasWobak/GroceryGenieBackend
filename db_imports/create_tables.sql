4e18dea97bc0ee64158b5ca4ed6384d21b1435df
ThomasWobak <t1wobak@edu.aau.at>
2025-04-23 15:27:40 +0200

    create of database

---
+++ db_imports/create_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "shopping_list" (
+  "id" SERIAL PRIMARY KEY,
+  "creator_id" integer NOT NULL,
+  "title" varchar NOT NULL,
+  "symbol" varchar,
+  "item_count" integer DEFAULT 0,
+  "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE "item" (
+  "id" SERIAL PRIMARY KEY,
+  "shopping_list_id" integer NOT NULL,
+  "name" varchar NOT NULL,
+  "amount" float,
+  "unit" varchar(10),
+  "last_update" timestamp DEFAULT (CURRENT_TIMESTAMP),
+  "recurrence_days" integer,
+  "active" boolean DEFAULT true
+);
+
+CREATE TABLE "user" (
+  "id" SERIAL PRIMARY KEY,
+  "auth0_key" varchar UNIQUE,
+  "username" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "profile_picture" varchar,
+  "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE "user_has_shopping_list" (
+  "shopping_list_id" integer NOT NULL,
+  "user_id" integer NOT NULL,
+  PRIMARY KEY ("shopping_list_id", "user_id")
+);
+
+ALTER TABLE "shopping_list" ADD FOREIGN KEY ("creator_id") REFERENCES "user" ("id");
+
+ALTER TABLE "item" ADD FOREIGN KEY ("shopping_list_id") REFERENCES "shopping_list" ("id");
+
+ALTER TABLE "user_has_shopping_list" ADD FOREIGN KEY ("shopping_list_id") REFERENCES "shopping_list" ("id");
+
+ALTER TABLE "user_has_shopping_list" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
